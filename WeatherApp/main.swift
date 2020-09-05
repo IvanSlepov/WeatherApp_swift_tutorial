3769d428bd8bfa125609bbfc45f5ae54f325aaf7
Ivan Sliepov <slepow1985@gmail.com>
2020-09-05 22:22:52 +0300

    Initial Commit

---
+++ WeatherApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WeatherApp
+//
+//  Created by Ivan Sliepov on 05.09.2020.
+//  Copyright © 2020 Ivan Sliepov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
